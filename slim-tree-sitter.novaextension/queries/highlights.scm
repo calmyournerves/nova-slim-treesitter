0d062f61691fe5da2bbfd3bf50ee78874e2fc4eb
Samuel Sieg <samuel.sieg@crafted.ch>
2024-07-06 11:18:08 +0200

    attr_value → attr_value_quoted

--- slim-tree-sitter.novaextension/queries/highlights.scm
+++ slim-tree-sitter.novaextension/queries/highlights.scm
@@ -2,7 +2,7 @@
 (doctype) @processing.doctype
 (tag_name) @tag.name
 (attr_name) @tag.attribute.name
-(attr_value) @tag.attribute.value
+(attr_value_quoted) @tag.attribute.value
 (attr) @identifier.variable
 (attr_shortcut_class) @style.selector.identifier.class
 (attr_shortcut_id) @style.selector.identifier.id
